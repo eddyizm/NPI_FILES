4be889691fc3d0e6517f6b794a0461b36bc6d566
eddyizm <eddyizm@gmail.com>
2023-11-09 20:07:21 -0800

    moved download script, added dummy env file, added function to scrape info page, including sample

---
+++ src/get_download_test.py
@@ -0,0 +1,41 @@
+import requests
+
+BASE_URL='https://download.cms.gov'
+BASE_PATH='nppes'
+INFO_URL=f'{BASE_URL}/{BASE_PATH}/NPI_Files.html'
+
+
+def download_file(url, target_path):
+    # TODO implement check to make we the file doesn't already exists or we already didn't DL and dumped it into the db.
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+
+    with open(target_path, 'wb') as f:
+        f.write(response.content)
+    print("Download completed!")
+
+
+def download_deactivated_npi_report_zip():
+    """'target_path' is the directory of your project"""
+    # TODO Needs a way to change the URL every month.
+    # This URL will change every second Monday of the month.
+    file_name = 'NPPES_Deactivated_NPI_Report_100923.zip' # filenamne to be generated dynamically from scrape
+    url = f'{BASE_URL}/{BASE_PATH}/{file_name}'
+    target_path = f'tmp/{file_name}'
+
+    download_file(url, target_path)
+
+
+def get_page_contents(url: str) -> str:
+    """Return main page contents as text. grep results to find pattern or use beautitul soup
+        This should allow use to find all the file names we need, check against the DB or some other data store to see if we have
+        received that file yet and retrieve to extract the data.
+    """
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page_contents(INFO_URL) # redirected output to page.html for sample
+    download_deactivated_npi_report_zip()
