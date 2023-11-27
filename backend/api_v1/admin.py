fd889427d073200a6b6a031973a73f15ee7118a9
eddyizm <eddyizm@gmail.com>
2023-11-26 18:05:59 -0800

    linter nag

--- backend/api_v1/admin.py
+++ backend/api_v1/admin.py
@@ -5,3 +5,4 @@ from api_v1.models import DownloadURL
 @admin.register(DownloadURL)
 class DownloadURLAdmin(admin.ModelAdmin):
     list_display = ['file_name', 'created_by', 'created_at', 'url', 'downloaded']
+    
