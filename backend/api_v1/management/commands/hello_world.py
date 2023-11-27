b3588fb80c2b2686fd5eff9c9f9da74c6c01548d
eddyizm <eddyizm@gmail.com>
2023-11-26 16:02:26 -0800

    updated requirements and sample command

--- backend/api_v1/management/commands/hello_world.py
+++ backend/api_v1/management/commands/hello_world.py
@@ -2,10 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
+    '''doc link https://docs.djangoproject.com/en/4.2/howto/custom-management-commands/'''
     help = "Closes the specified poll for voting"
 
     # def add_arguments(self, parser):
     #     parser.add_argument("poll_ids", nargs="+", type=int)
-
+    
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("HELLO WORLD"))
